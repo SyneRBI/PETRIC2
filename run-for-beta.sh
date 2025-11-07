82a566e367d710d8f79da12803871448adfec96e
Edoardo Pasca <14138589+paskino@users.noreply.github.com>
2025-11-07 14:18:52 +0000

    run for multiple betas

--- run-for-beta.sh
+++ run-for-beta.sh
@@ -1,9 +1,10 @@
 #! /bin/sh
 
 . /opt/SIRF-SuperBuild/INSTALL/bin/env_sirf.sh
-
-
-# for beta in 1 10 100 1000; do
-for beta in 10; do
-  python run_LBFGSBPC.py Siemens_Vision600_ZrNEMAIQ --updates 4 --interval 2 --beta $beta
+cd 
+cd PETRIC2
+phantom="Siemens_Vision600_ZrNEMAIQ"
+for beta in 10 100 1000; do
+# for beta in 1; do
+  python run_LBFGSBPC.py $phantom --updates 400 --interval 100 --beta $beta > output_${phantom}_LBFGSBPC_beta_${beta}.log 2>&1
 done
