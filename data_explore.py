8807ecefc797eaeb7241847240c1f55429d728e9
Edoardo Pasca <14138589+paskino@users.noreply.github.com>
2025-11-14 17:13:58 +0000

    changes

--- data_explore.py
+++ data_explore.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import os
 import numpy as np
 from extract_data_slices import extract_data_slices
-
+from SIRF_data_preparation.preferred_scaling import preferred_scaling
 
 def read_interfile_header(fname):
     """"""""
@@ -21,8 +21,11 @@ def read_interfile_header(fname):
                 print(f"Index: {index}, Size: {size}")
                 shape[index-1] = size
     return tuple(shape[::-1])
+
+
+
 iter = 400
-num_phantom = 1
+num_phantom = 0
 beta = 1
 phantoms = ['Siemens_mMR_ACR', 'Siemens_mMR_NEMA_IQ', 'GE_D690_NEMA_IQ']
 shapes = [(127, 285, 285), (127, 200, 200), (127, 344, 344)]
@@ -58,18 +61,19 @@ def read_reconstructed_image(fname):
 recons = {}
 up = 0
 sup = np.inf
-# betas = [1,10,100,1000]
-betas = [1,10,100,1000]
+betas = [1] * 3
+# betas = ["1","1e-1","1e-2"]
 for b in betas:
     dirname = f'output/{phantom}/{algo}/{b}/'
     fname = f'{dirname}/{algo}'
+    print (fname)
 
     arr = read_reconstructed_image(fname)
     print (fname, arr.shape, arr.min(), arr.max())
     recons[b] = arr
 
     lp = 0
-    nnp = np.percentile(arr, 99.)
+    nnp = np.percentile(arr, 99.9)
     up = nnp if nnp > up else up
     sup = nnp if nnp < sup else sup
 # up = 0.0027166688
@@ -80,21 +84,22 @@ OSEM_dir = f"/mnt/share-public/petric-wip/petric2/{phantom}/"
 OSEM_fname = f"{OSEM_dir}/OSEM_image"
 osem_arr = read_reconstructed_image(OSEM_fname)
 
-nnp = np.percentile(arr, 99.)
-up = nnp if nnp > up else up
-sup = nnp if nnp < sup else sup
+# nnp = np.percentile(arr, 99.)
+# up = nnp if nnp > up else up
+# sup = nnp if nnp < sup else sup
 
-# %%
-fig, other = plt.subplots(3,2)
+
+#%%
+fig, other = plt.subplots(2,2, figsize=(8,12))
 DATA_SLICES = extract_data_slices()
 slices = DATA_SLICES[phantom]['transverse_slice']
 # up = 0.9e-5
-top = sup
+top = up
 plt.suptitle(f'{algo}/{phantom}: slice={slices} sfs=1e-1 up={top:.1e}')
 cmap = "magma"
 
 for i, b in enumerate(betas):
-     ax = plt.subplot(3,2,i+1)
+     ax = plt.subplot(2,2,i+1)
      f = ax.imshow(recons[b][slices,:,:], cmap=cmap, vmax=top, vmin=lp)
      ax.set_title(f'Beta: {b}')
     #  plt.colorbar(
@@ -102,18 +107,22 @@ for i, b in enumerate(betas):
     #      ax=ax)
      ax.axis('off')
 
-ax = plt.subplot(3,2,i+2)
+ax = plt.subplot(2,2,i+2)
 ax.imshow(osem_arr[slices,:,:], cmap=cmap, vmax=top, vmin=lp)
 ax.set_title(f'OSEM')
 # plt.colorbar(ax.imshow(osem_arr[slices,:,:], cmap=cmap, vmax=top, vmin=lp), ax=ax)
 ax.axis('off')
-ax = plt.subplot(3,2,i+3)
-ax.axis('off')
+# ax = plt.subplot(2,2,i+3)
+# ax.axis('off')
 
 cax = plt.axes([0.915, 0.02, 0.02, 0.907])  # Adjust the position of the colorbar
 fig.colorbar(f, orientation='vertical', 
             #  use_gridspec=True,
              cax=cax)
+# Remove tight_layout and use subplots_adjust to control spacing precisely
+plt.subplots_adjust(wspace=0.3, hspace=0.02, 
+                    left=0.02, right=0.91, 
+                    top=0.9, bottom=0.02)
 plt.show()
 #%%
 
