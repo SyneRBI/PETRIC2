354dc695588f314de78e54bb43926c5618dbbb1f
Kris Thielemans <KrisThielemans@users.noreply.github.com>
2025-11-15 14:38:06 +0000

    use different preconditioner for LBFGSPBC (#16)
    Much faster convergence when using diag(Hessian_of_prior) as opposed to Hessian_of_prior.1. Sadly, we need to use STIR python for that ATM.
    
    Also some minor fixes on plotting.

--- SIRF_data_preparation/run_LBFGSBPC.py
+++ SIRF_data_preparation/run_LBFGSBPC.py
@@ -24,21 +24,79 @@ import csv
 from pathlib import Path
 
 import matplotlib.pyplot as plt
+import stir  # stir Python interface
 from docopt import docopt
 
-import sirf.STIR as STIR
-from petric import OUTDIR, SRCDIR, MetricsWithTimeout, get_data
+import sirf.STIR as STIR  # SIRF python interface to STIR
+from petric import OUTDIR, SRCDIR, get_data
+from sirf.contrib.LBFGSBPC.LBFGSBPC import LBFGSBPC
 from sirf.contrib.partitioner import partitioner
 from SIRF_data_preparation import data_QC
 from SIRF_data_preparation.dataset_settings import get_settings
 
-import numpy
-from sirf.contrib.LBFGSBPC.LBFGSBPC import LBFGSBPC
 # %%
 
 STIR.AcquisitionData.set_storage_scheme('memory')
 _ = STIR.MessageRedirector('info.txt', 'warnings.txt', 'errors.txt')
 STIR.set_verbosity(0)
+
+
+# %% STIR functions for functionality still missing in SIRF
+# the sirf.STIR.RelativeDifferencePrior does not yet expose all functions available in STIR 6.3
+# In particular, we need the diagonal of the Hessian of the prior.
+# The following contains some ugly/unsafe work-arounds
+def sirf_to_stir(image: STIR.ImageData) -> stir.FloatVoxelsOnCartesianGrid:
+    r"""Convert sirf.STIR.ImageData to a stir object
+
+    Warning: writes to tmp.* in current directory (could be done better)
+    """
+    image.write('tmp.hv')
+    return stir.FloatVoxelsOnCartesianGrid.read_from_file('tmp.hv')
+
+
+def stir_to_sirf(image: stir.FloatVoxelsOnCartesianGrid) -> STIR.ImageData:
+    r"""Convert stir image to sirf.STIR.ImageData
+
+    Warning: writes to tmp.* in current directory (could be done better)
+    """
+    image.write_to_file('tmp.hv')
+    return STIR.ImageData('tmp.hv')
+
+
+def construct_stir_RDP(sirf_prior: STIR.RelativeDifferencePrior, initial_image: STIR.ImageData):
+    '''
+    Construct a stir smoothed Relative Difference Prior (RDP), given input from a sirf.STIR one
+
+    `initial_image` is used to determine a smoothing factor (epsilon).
+    `kappa` is used to pass voxel-dependent weights.
+    '''
+    prior = stir.GibbsRelativeDifferencePenalty3DFloat()
+    # need to make it differentiable
+    prior.set_epsilon(sirf_prior.get_epsilon())
+    prior.set_penalisation_factor(sirf_prior.get_penalisation_factor())
+    if sirf_prior.get_kappa() is not None:
+        prior.set_kappa_sptr(sirf_to_stir(sirf_prior.get_kappa()))
+    prior.set_up(sirf_to_stir(initial_image))
+    return prior
+
+
+def construct_preconditioner(initial: STIR.ImageData, obj_fun: STIR.ObjectiveFunction,
+                             sirf_prior: STIR.RelativeDifferencePrior) -> STIR.ImageData:
+    r"""Construct a preconditioner for LBFGSB-PC
+
+    Tsai's paper uses (-H.1). However, for the RDP, this does not work very well. We have better
+    results when using (-H_loglikelihood.1 + diag(H_prior))
+    """
+    stir_initial = sirf_to_stir(initial)
+    # sirf_prior = obj_fun.get_prior() # returns object of type Prior, which isn't good enough
+    stir_prior = construct_stir_RDP(sirf_prior, initial)
+    diag = stir_initial.clone()
+    stir_prior.compute_Hessian_diagonal(diag, stir_initial)
+    diag = stir_to_sirf(diag)
+    sirf_prior.set_penalisation_factor(0)
+    ll_precon = obj_fun.multiply_with_Hessian(initial, initial.get_uniform_copy(1)) * -1
+    sirf_prior.set_penalisation_factor(stir_prior.get_penalisation_factor())
+    return ll_precon + diag
 
 
 # %%
@@ -78,34 +136,37 @@ print("initial_image:", initial_image_name)
 print("outdir:", outdir)
 print("interval:", interval)
 
-num_subsets=1
-_, _, obj_funs = partitioner.data_partition(data.acquired_data, data.additive_term, data.mult_factors,
-                                                    num_subsets,
-                                                    initial_image=data.OSEM_image)
+num_subsets = 1
+initial = data.OSEM_image
+_, _, obj_funs = partitioner.data_partition(data.acquired_data, data.additive_term, data.mult_factors, num_subsets,
+                                            initial_image=initial)
 obj_fun = obj_funs[0]
-data.prior.set_up(data.OSEM_image)
+data.prior.set_up(initial)
 # acq_model = acq_models[0]
 obj_fun.set_prior(data.prior)
 
-algo = LBFGSBPC(obj_fun, initial=initial_image, update_objective_interval=interval)
+precond = construct_preconditioner(initial, obj_fun, data.prior)
+algo = LBFGSBPC(obj_fun, initial=initial_image, update_objective_interval=interval,
+                save_intermediate_results_path=str(outdir))
+algo.set_preconditioner(precond)
 # %%
 algo.run(iterations=num_updates)
 # %%
 algo.get_output().write(str(outdir / "LBFGSBPC.hv"))
 # %%
-csv = csv.writer((self.outdir / 'objectives.csv').open("w", buffering=1))
-csv.writerow(("iter", "objective"))
-for i,l in zip(algo.iterations, algo.loss):
-    csv.writerow((i,l))
+cvswriter = csv.writer((outdir / 'objectives.csv').open("w", buffering=1))
+cvswriter.writerow(("iter", "objective"))
+for i, l in zip(algo.iterations, algo.loss):
+    cvswriter.writerow((i, l))
 
 # %%
 fig = plt.figure()
-data_QC.plot_image(algo.get_output(), **settings.slices)
+data_QC.plot_image(algo.get_output(), **settings.slices, vmax=settings.get('vmax', None))
 fig.savefig(outdir / "LBFGSBPC_slices.png")
 # plt.show()
 # %%
 print(algo.iterations)
 print(algo.loss)
 fig = plt.figure()
-plt.plot(algo.iterations, recon.loss)
+plt.plot(algo.iterations, algo.loss)
 fig.savefig(outdir / "LBFGSBPC_objective.png")
