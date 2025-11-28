c4b1e2c4093d04d0f13d30e5b77ba55282686c13
Edoardo Pasca <14138589+paskino@users.noreply.github.com>
2025-11-28 08:11:22 +0000

    run in batch mode

--- run-for-beta.sh
+++ run-for-beta.sh
@@ -1,16 +1,16 @@
 #! /bin/sh
 
 . /opt/SIRF-SuperBuild/INSTALL/bin/env_sirf.sh
-cd 
-cd PETRIC2
+# cd 
+# cd PETRIC2
 # phantoms="Siemens_mMR_ACR GE_D690_NEMA_IQ Siemens_mMR_NEMA_IQ Siemens_Vision600_ZrNEMAIQ"
-phantoms="Siemens_mMR_ACR"
+phantoms="Siemens_Vision600_Hoffman"
 for phantom in $phantoms; do
 #  for beta in 1 10 100 1000; do
-for beta in 1 ; do
+for beta in '1.5' ; do
   # python run_LBFGSBPC.py $phantom --updates 400 --interval 100 --beta $beta > output/output_${phantom}_LBFGSBPC_beta_${beta}.log 2>&1
   echo -n "Running MaGeZ for phantom $phantom with beta $beta ..."
-  python run_MaGeZ.py $phantom --updates 400 --interval 100 --beta $beta > output/output_${phantom}_MaGeZ_beta_${beta}.log 2>&1
+  python run_MaGeZ.py $phantom --updates 400 --interval 100 --beta $beta > output/output_${phantom}_MaGeZ_betas.log 2>&1
   echo " Done"
  done
 done
