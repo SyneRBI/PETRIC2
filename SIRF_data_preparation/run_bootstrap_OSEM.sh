c7bbb8db3fb3d2bd48a454e5c7456b3c1db716d5
Kris Thielemans <k.thielemans@ucl.ac.uk>
2026-01-16 14:48:43 +0000

    add (mandatory) option to say where data comes from

--- SIRF_data_preparation/run_bootstrap_OSEM.sh
+++ SIRF_data_preparation/run_bootstrap_OSEM.sh
@@ -6,9 +6,10 @@ export PETRIC_SKIP_DATA=1
 dataset=$1
 sfs="$2"
 reps="$3"
+srcdir="$4"
 for sf in $sfs; do
  for rep in $reps; do
-   python -m SIRF_data_preparation.noise_bootstrap --dataset=${dataset}  --scale_factor=${sf} --outname=${dataset}_${sf}_${rep}
+   python -m SIRF_data_preparation.noise_bootstrap --dataset=${dataset} --srcdir="${srcdir}" --scale_factor=${sf} --outname=${dataset}_${sf}_${rep}
    python -m SIRF_data_preparation.data_QC --srcdir=output/${dataset}_${sf}_${rep} --dataset=${dataset} --skip_sino_profiles --skip_VOI_plots --no_plot_wait
  done
 done
