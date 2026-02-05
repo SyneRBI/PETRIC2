1c7dc0df9f1ea03af15576a3062e0a180c6f23cc
Casper da Costa-Luis <casper.dcl@physics.org>
2026-02-05 23:18:42 +0000

    update Mediso data prep

--- SIRF_data_preparation/Mediso_NEMA_IQ/run_LBFGSBPC.sh
+++ SIRF_data_preparation/Mediso_NEMA_IQ/run_LBFGSBPC.sh
@@ -1,28 +1,30 @@
 # STFC hard-wired location
 datasrc=/mnt/share-public/petric
+export PETRIC_SRCDIR=data
+export PETRIC_OUTDIR=output
 dataset=Mediso_NEMA_IQ
 noise_sf=0.01
-bash -vx SIRF_data_preparation/run_bootstrap_OSEM.sh $dataset ${noise_sf} 1 ~/devel/PETRIC2/data1/$dataset
+sf=0.06
+
+bash -vx SIRF_data_preparation/run_bootstrap_OSEM.sh $dataset ${noise_sf} 1 $datasrc/1/$dataset
 # python -m SIRF_data_preparation.create_initial_images data/$dataset
 rm -rf data/${dataset}
 mv output/${dataset}_${noise_sf}_1 data/${dataset}
+
 cp -rp "${datasrc}/1/$dataset/PETRIC" data/${dataset}/
 rm data/${dataset}/PETRIC/reference*
 # threshold any (very small) negatives in OSEM
 pushd data/${dataset}
-mkdir tmp; mv OSEM_image.*v tmp;stir_math --min-threshold 0 --including-first OSEM_image tmp/OSEM_image.hv; rm -rf tmp
+mkdir tmp && mv OSEM_image.*v tmp && stir_math --min-threshold 0 --including-first OSEM_image tmp/OSEM_image.hv && rm -rf tmp
 popd
 
-sf=0.06
 python -m SIRF_data_preparation.run_LBFGSBPC --penalisation_factor_multiplier=$sf --outreldir=LBFGSBPC${sf} --initial_FWHM=5 --interval=20 --updates=80 ${dataset}
 python -m SIRF_data_preparation.run_LBFGSBPC --penalisation_factor_multiplier=$sf --outreldir=LBFGSBPC${sf}_cont1 --initial_image=output/${dataset}/LBFGSBPC${sf}/iter_final.hv --interval=2 --updates=80 ${dataset}
 
 pushd data/${dataset}
-beta=$(PETRIC_SRCDIR="${datasrc}"/1 python -m SIRF_data_preparation.print_penalisation_factor --dataset=${dataset})
 rm -rf *ahv *txt orgkappa PETRIC/*ahv
-echo "print($beta * $sf)"| python -q - > penalisation_factor.txt
-echo "penalisation factor $(cat penalisation_factor.txt) = $beta * $sf"
 popd
+cp output/${dataset}/LBFGSBPC${sf}/penalisation_factor.txt data/${dataset}
 
 ref_image="data/${dataset}/PETRIC/reference_image.hv"
 stir_math "$ref_image" output/${dataset}/LBFGSBPC${sf}_cont1/iter_final.hv
@@ -34,8 +36,6 @@ compare_image output/${dataset}/LBFGSBPC_final/iter_final.hv "$ref_image"
 python -m SIRF_data_preparation.data_QC --dataset=${dataset}
 
 pushd data
-# warning This did not work, as ${dataset} was itself a symbolic link
-# mv ${dataset} ${datasrc}/2/
-mv ${dataset}_${noise_sf}_1 ${datasrc}/2/
+mv ${dataset} ${datasrc}/2/
 ln -s ${datasrc}/2/${dataset}
 popd
