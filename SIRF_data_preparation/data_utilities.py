1bba346ab5425ba4c00bef95a5fd17a72972d3d6
Kris Thielemans <k.thielemans@ucl.ac.uk>
2026-02-23 18:15:42 +0000

    split Siemens list-mode handling function into two
    make it useful for non-Siemens data as well

--- SIRF_data_preparation/data_utilities.py
+++ SIRF_data_preparation/data_utilities.py
@@ -1,8 +1,8 @@
-'''Library of Siemens data preparation utilities.'''
+'''Library of data preparation utilities.'''
 # Authors: Ashley Gillman, Kris Thielemans, Evgueni Ovtchinnikov
 # Licence: Apache-2.0
 # Copyright (C) 2021 Commonwealth Scientific and Industrial Research Organisation
-# Copyright (C) 2024, 2025 University College London
+# Copyright (C) 2024, 2025, 2026 University College London
 # Copyright (C) 2024 STFC, UK Research and Innovation
 import importlib
 import logging
@@ -66,9 +66,9 @@ def prepare_challenge_Siemens_data(data_path, challenge_data_path, intermediate_
     f_norm: Siemens normalisation data file suffix
     f_stir_norm: STIR normalisation data file name
     f_template: template for prompts file name
-    f_prompts: prompts file name
-    f_multfactors: multfactors file name
-    f_additive: additive term file name
+    f_prompts: IGNORED
+    f_multfactors: IGNORED
+    f_additive: IGNORED
     f_randoms: estimated randoms file name
     f_af: attenuation factor file name
     f_acf: attenuation correction factor file name
@@ -77,8 +77,8 @@ def prepare_challenge_Siemens_data(data_path, challenge_data_path, intermediate_
     stop: end time for data acquisition
     '''
 
-    logging.info(f"Start time for data: {start} sec")
-    logging.info(f"End time for data: {stop} sec")
+    logger.info(f"Start time for data: {start} sec")
+    logger.info(f"End time for data: {stop} sec")
 
     f_listmode = os.path.join(data_path, f_root + f_listmode)
     f_siemens_attn_image = os.path.join(data_path, f_root + f_mumap)
@@ -87,15 +87,6 @@ def prepare_challenge_Siemens_data(data_path, challenge_data_path, intermediate_
     f_siemens_norm_header = f_siemens_norm + '.hdr'
     f_stir_norm_header = os.path.join(intermediate_data_path, f_stir_norm)
     f_stir_attn_header = os.path.join(intermediate_data_path, f_root + f_attn)
-    f_prompts = os.path.join(challenge_data_path, f_prompts)
-    f_multfactors = os.path.join(challenge_data_path, f_multfactors)
-    f_additive = os.path.join(challenge_data_path, f_additive)
-    f_randoms = os.path.join(intermediate_data_path, f_randoms)
-    f_af = os.path.join(intermediate_data_path, f_af)
-    f_acf = os.path.join(intermediate_data_path, f_acf)
-    f_scatter = os.path.join(intermediate_data_path, f_scatter)
-    f_info = os.path.join(intermediate_data_path, 'info.txt')
-    f_warn = os.path.join(intermediate_data_path, 'warn.txt')
 
     if os.path.exists(f_siemens_attn_image):
         if data_path != intermediate_data_path:
@@ -107,48 +98,99 @@ def prepare_challenge_Siemens_data(data_path, challenge_data_path, intermediate_
     if data_path != intermediate_data_path:
         shutil.copy(f_siemens_norm, intermediate_data_path)
     fix_siemens_norm_EOL(f_siemens_norm_header, f_stir_norm_header)
+    prepare_challenge_STIR_data(challenge_data_path, intermediate_data_path, f_listmode, f_stir_attn_header,
+                                f_stir_norm, f_template, f_randoms, f_af, f_acf, f_scatter, [start, stop])
 
-    # engine's messages go to files, except error messages, which go to stdout
-    _ = pet.MessageRedirector(f_info, f_warn)
 
-    # select acquisition data storage scheme
-    pet.AcquisitionData.set_storage_scheme('memory')
+def prepare_challenge_STIR_data(challenge_data_path, intermediate_data_path, f_listmode, f_attn_image, f_stir_norm,
+                                f_template, f_randoms, f_af='attenuation_factor', f_acf='attenuation_correction_factor',
+                                f_scatter='scatter', start_stop: list[int] | None = None):
+    '''Prepares list-mode data etc for SyneRBI PETRIC via sirf.STIR
+
+    challenge_data_path: path to final prepared data
+    intermediate_data_path: path to folder for temporary data
+    f_listmode: (full) list-mode data filename
+    f_attn_image: (full) mu-map filename
+    f_stir_norm: (full) STIR normalisation data file name, can be STIR normalisation factors as a sinogram
+    f_template: (full) template for prompts file name
+    f_randoms: estimated randoms file name (will be prefixed by intermediate_data_path)
+    f_af: attenuation factor file name (will be prefixed by intermediate_data_path)
+    f_acf: attenuation correction factor file name (will be prefixed by intermediate_data_path)
+    f_scatter: scatter estimate file name (will be prefixed by intermediate_data_path)
+    start_stop: start/end time for data acquisition (default: None means "use all data")
+    '''
+    # f_info = os.path.join(intermediate_data_path, 'info.txt')
+    # f_warn = os.path.join(intermediate_data_path, 'warn.txt')
+
+    f_prompts = os.path.join(challenge_data_path, 'prompts.hs')
+    f_multfactors = os.path.join(challenge_data_path, 'mult_factors.hs')
+    f_additive = os.path.join(challenge_data_path, 'additive_term.hs')
+    f_af = os.path.join(intermediate_data_path, f_af)
+    f_acf = os.path.join(intermediate_data_path, f_acf)
+    f_scatter = os.path.join(intermediate_data_path, f_scatter)
 
     # read acquisition data template
     acq_data_template = pet.AcquisitionData(f_template)
-    logger.info(acq_data_template.norm())
 
+    # filename as written by ListmodeToSinograms
     output_prefix = os.path.join(intermediate_data_path, "prompts")
+    f_prompts_tmp = output_prefix + '_f1g1d0b0.hs'
+    if os.path.exists(f_prompts_tmp) and os.path.exists(f_randoms):
+        logger.info("Using existing prompts and randoms data...")
+        print("Using existing prompts and randoms data.")
+        prompts = pet.AcquisitionData(f_prompts_tmp)
+        randoms = pet.AcquisitionData(f_randoms)
+    else:
+        listmode_data = pet.ListmodeData(f_listmode)
 
-    listmode_data = pet.ListmodeData(f_listmode)
+        logger.info("Processing listmode data...")
+        logger.info(f"Start/step time: {start_stop}")
+        logger.info(f"Output prefix: {output_prefix}")
+        # create listmode-to-sinograms converter object
+        lm2sino = pet.ListmodeToSinograms()
+        lm2sino.set_input(listmode_data)
+        lm2sino.set_output_prefix(output_prefix)
+        lm2sino.set_template(acq_data_template)
+        if start_stop is not None:
+            lm2sino.set_time_interval(start_stop[0], start_stop[1])
+        lm2sino.set_up()
+        lm2sino.process()
+        prompts = lm2sino.get_output()
+        if f_randoms is None:
+            randoms = lm2sino.estimate_randoms()
+        else:
+            randoms = pet.AcquisitionData(f_randoms)
 
-    logger.info("Processing listmode data...")
-    logger.info(f"Start time: {start} sec. End time: {stop} sec.")
-    logger.info(f"Output prefix: {output_prefix}")
-    # create listmode-to-sinograms converter object
-    lm2sino = pet.ListmodeToSinograms()
-    lm2sino.set_input(listmode_data)
-    lm2sino.set_output_prefix(output_prefix)
-    lm2sino.set_template(acq_data_template)
-    lm2sino.set_time_interval(start, stop)
-    lm2sino.set_up()
-    lm2sino.process()
-
-    prompts = lm2sino.get_output()
-    randoms = lm2sino.estimate_randoms()
     logger.info('data shape: %s' % repr(prompts.shape))
     logger.info('prompts norm: %f' % prompts.norm())
     logger.info('randoms norm: %f' % randoms.norm())
 
+    f_randoms = os.path.join(intermediate_data_path, 'randoms.hs')
     logger.info(f'writing prompts to {f_prompts} and randoms to {f_randoms}')
     prompts.write(f_prompts)
     randoms.write(f_randoms)
 
-    asm = pet.AcquisitionSensitivityModel(f_stir_norm_header)
+    try:
+        # try to read STIR norm factors is acquisition data (which are 1/eff_factors)
+        stir_norm_factors = pet.AcquisitionData(f_stir_norm)
+        asm = pet.AcquisitionSensitivityModel(stir_norm_factors.power(-1))
+    except:
+        # try to read as STIR supported vendor-norm file
+        asm = pet.AcquisitionSensitivityModel(f_stir_norm)
 
-    if os.path.exists(f_siemens_attn_image):
-        attn_image = pet.ImageData(f_stir_attn_header)
-        af, acf = pet.AcquisitionSensitivityModel.compute_attenuation_factors(prompts, attn_image)
+    if os.path.exists(f_attn_image):
+        attn_image = pet.ImageData(f_attn_image)
+        logger.info('computing attenuation factors')
+        # can't use next line yet, as it doesn't allow setting the acq_model_for_attn
+        # af, acf = pet.AcquisitionSensitivityModel.compute_attenuation_factors(prompts, attn_image)
+        acq_model_for_attn = pet.AcquisitionModelUsingParallelproj()
+        asm_attn = pet.AcquisitionSensitivityModel(attn_image, acq_model_for_attn)
+        af = prompts.allocate(1)
+        logger.info('computing attenuation factors')
+        asm_attn.set_up(af)
+        asm_attn.unnormalise(af)
+        del asm_attn
+        acf = af.power(-1)
         logger.info('norm of the attenuation factor: %f' % af.norm())
         logger.info('norm of the attenuation correction factor: %f' % acf.norm())
         logger.info(f'writing intermediate attenuation factors to {f_af}')
@@ -162,8 +204,10 @@ def prepare_challenge_Siemens_data(data_path, challenge_data_path, intermediate_
         se.set_randoms(randoms)
         se.set_asm(asm)
         se.set_attenuation_correction_factors(acf)
+        se.set_max_scale_value(1.5)
+        se.set_min_scale_value(.4)
         se.set_num_iterations(4)
-        se.set_OSEM_num_subsets(7)
+        se.set_OSEM_num_subsets(2)
         se.set_output_prefix(f_scatter)
         se.set_up()
         se.process()
