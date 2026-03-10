ed601894267a7176692864ac6cbe575d7ce547ac
Kris Thielemans <KrisThielemans@users.noreply.github.com>
2026-03-10 08:57:53 +0000

    More bootstrap (#83)
    * added GE_DMI3_Torso2, GE_DMI4_Torso2, extract common lines to script

---
+++ SIRF_data_preparation/GE_DMI4_NEMA_IQ2/run_LBFGSBPC.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+export PETRIC_SRCDIR=data
+export PETRIC_OUTDIR=output
+export dataset=GE_DMI4_NEMA_IQ2
+export template_dataset=GE_DMI4_NEMA_IQ
+export fullcounts_DIR=/mnt/share-public/petric/1/$template_dataset
+export sf=1
+export noise_sf=$(python -c "from SIRF_data_preparation.dataset_settings import preferred_scaling as s; print(s['$dataset'], end='')")
+SIRF_data_preparation/generate_dataset_with_bootstrapping.sh
+
+#pushd data
+#mv ${dataset} /mnt/share-public/petric/2/
+#ln -s /mnt/share-public/petric/2/${dataset}
+#popd
