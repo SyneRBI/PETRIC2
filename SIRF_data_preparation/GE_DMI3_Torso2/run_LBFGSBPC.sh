448610ad9d72e66a381ea9b44e6b74dc4a8b15fc
Kris Thielemans <k.thielemans@ucl.ac.uk>
2026-03-10 01:00:50 +0000

    fix location of DMI3_Torso fullcounts data

--- SIRF_data_preparation/GE_DMI3_Torso2/run_LBFGSBPC.sh
+++ SIRF_data_preparation/GE_DMI3_Torso2/run_LBFGSBPC.sh
@@ -1,10 +1,9 @@
 #! /bin/bash
-datasrc=/mnt/share/petric/wip/petric2/orgdata
 export PETRIC_SRCDIR=data
 export PETRIC_OUTDIR=output
 export dataset=GE_DMI3_Torso2
 export template_dataset=GE_DMI3_Torso
-export fullcounts_DIR=/mnt/share/petric/1/$template_dataset
+export fullcounts_DIR=/mnt/share-public/petric/1/$template_dataset
 export sf=1
 export noise_sf=$(python -c "from SIRF_data_preparation.dataset_settings import preferred_scaling as s; print(s['$dataset'], end='')")
 SIRF_data_preparation/generate_dataset_with_bootstrapping.sh
